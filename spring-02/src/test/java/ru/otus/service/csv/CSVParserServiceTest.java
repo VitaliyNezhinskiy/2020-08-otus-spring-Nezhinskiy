d126d4d61fd3171274b0cd1dd966f2a678c39c0c
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-09-11 18:08:20 +0300

    refactor CSVParserServiceTest to unit test

--- spring-02/src/test/java/ru/otus/service/csv/CSVParserServiceTest.java
+++ spring-02/src/test/java/ru/otus/service/csv/CSVParserServiceTest.java
@@ -7,7 +7,9 @@ import org.mockito.Mockito;
 import ru.otus.service.QuestionService;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 import static org.mockito.Mockito.when;
 
@@ -21,9 +23,9 @@ public class CSVParserServiceTest {
         QuestionService questionService = Mockito.mock(QuestionService.class);
 
         when(questionService.getReader()).thenReturn(new BufferedReader
-                (new InputStreamReader(
-                        this.getClass()
-                                .getResourceAsStream("/test_questions_with_answers.csv"))));
+                (new InputStreamReader(new ByteArrayInputStream(
+                        ("It has got one color for winter and summer. " +
+                                "What is it?,fir,beam,lightbulb,hare,1").getBytes(Charset.defaultCharset())))));
 
         CSVParserService csvParserService = new CSVParserService(questionService);
         Assertions.assertFalse(csvParserService.getCsvRecordList().isEmpty());
