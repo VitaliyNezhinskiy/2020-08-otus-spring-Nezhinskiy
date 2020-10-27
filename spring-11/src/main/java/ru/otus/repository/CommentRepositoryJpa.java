4974040bf1fdb56ec6155ead0f261e0fdb7aa0c5
Vitaliy <SeekerOfConfessor@gmail.com>
2020-10-27 05:45:29 +0300

    delete import

--- spring-11/src/main/java/ru/otus/repository/CommentRepositoryJpa.java
+++ spring-11/src/main/java/ru/otus/repository/CommentRepositoryJpa.java
@@ -1,7 +1,6 @@
 package ru.otus.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ru.otus.domain.Comment;
 
 import java.util.Optional;
